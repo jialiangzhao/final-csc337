584401abc7b367400520bbfc14b94909f4c2f023
jialiangzhao <66649758+jialiangzhao@users.noreply.github.com>
2021-08-04 20:14:08 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ var itemsSchema = new Schema({
   image:String,
   price:Number,
   stat:String,
-  deadLine:String,
-  current:Number
+  deadLine:Object,
+  buyer:String
 });
 var items= mongoose.model('items',itemsSchema);
 
@@ -49,8 +49,9 @@ var userSchema = new Schema({
   head:String,
   salt:String,
   hash:String,
+  addMoney:Number,
   listings: [String],
-  incomeList:[String]
+  incomeList:[Object]
 });
 var user= mongoose.model('user',userSchema);
 
@@ -60,6 +61,8 @@ var incomeSchema = new Schema({
   kind:String,
   money:Number,
   inOrOut:String,
+  date:Object,
+  time:Number
 });
 var income= mongoose.model('income',incomeSchema);
 
@@ -109,6 +112,9 @@ function home(req,res,next){
 
 app.use('/home.html',home);
 app.use('/post.html',home);
+app.use('/auction.html',home);
+app.use('/money.html',home);
+
 
 app.use('/',express.static('public_html'));
 
@@ -129,10 +135,10 @@ app.get('/login/:u/:p',function(req,res) {
         if(results[0].hash==hStr){
           let sessionKey=Math.floor(Math.random()*1000);
           sessionKeys[u]=[sessionKey,Date.now()];
-          res.cookie('login',{username:u,key:sessionKey},{maxAge:200000});
+          res.cookie('login',{username:u,key:sessionKey,incomeList:results[0].incomeList},{maxAge:200000});
           res.send(results);
         }
-        });
+      });
 
     }else{
       res.send('Incorrect login, please try again...');
@@ -148,7 +154,8 @@ app.post('/new',upload.single('photo'),function(req,res,next) {
  
   let u=req.body.name;
   let p=req.body.password;
-  if(u=="" || p=="" || req.file.filename==undefined){
+  let add=req.body.addMoney;
+  if(u=="" || p=="" || req.file.filename==undefined || add==""){
     res.send('no name or password or avatar, do again');
   }
 
@@ -161,7 +168,7 @@ app.post('/new',upload.single('photo'),function(req,res,next) {
         if(err) throw err;
         let hStr = hash.toString('base64');
         var account= new user({'username':u,'salt':salt,
-       "head":req.file.filename,'hash':hStr});
+       'hash':hStr,"head":req.file.filename,'addMoney':add});
         account.save(function(err){if(err) console.log('an error')});
         res.redirect("index.html");
         });
@@ -194,33 +201,50 @@ app.get('/view/:thing/:name',function(req,res) {
   let thing=req.params.thing;
   let name=req.params.name;
   var array=[];
-  user.find({}).exec(function(error,results){
-    
-    for(i in results){
-      if(results[i].username==name){
+  user.find({username:name}).exec(function(error,results){
+
+      if(results.length!=0){
         if(thing=="listings"){
-          array=results[i].listings;
+          array=results[0].listings;
+          items.find({}).exec(function(error1,results1){
+            let word="[";
+           
+            for(i in array){
+              for(j in results1){
+                if(results1[j]._id==array[i]){
+                  word+=JSON.stringify(results1[j],null,2)+",";
+                }
+              }
+           }
+           if(word=="["){
+             word+="]";
+           }else{
+           word=word.substring(0, word.length - 1) +"]";}
+            res.send(word);
+          });
+
+          
         }else if(thing=="purchases"){
-          array=results[i].purchases;
+
+          items.find({stat:"SOLD"}).exec(function(error2,results2){
+            let word="[";
+              for(j in results2){
+                if(results2[j].buyer==name){
+                  word+=JSON.stringify(results2[j],null,2)+",";
+                }
+              }
+           if(word=="["){
+             word+="]";
+           }else{
+           word=word.substring(0, word.length - 1) +"]";}
+            res.send(word);
+          });
         }
+      }else{
+        res.send("view error");
       }
-    }
-    items.find({}).exec(function(error1,results1){
-      let word="[";
-     
-      for(i in array){
-        for(j in results1){
-          if(results1[j]._id==array[i]){
-            word+=JSON.stringify(results1[j],null,2)+",";
-          }
-        }
-     }
-     if(word=="["){
-       word+="]";
-     }else{
-     word=word.substring(0, word.length - 1) +"]";}
-      res.send(word);
-    });
+    
+    
   });
 });
 
@@ -229,15 +253,17 @@ app.get('/view/:thing/:name',function(req,res) {
 app.post('/buy/:id/:name',function(req,res) {
   let tid=req.params.id;
   let name=req.params.name;
+  
   items.find({_id:tid})
   .exec(function(error,results){
-    results[0].stat="SOLD";
-    results[0].save(function(err){if(err) console.log("a save errorr");});
-    user.find({username:name})
-    .exec(function(error,result){
-      result[0].purchases.push(results[0]._id);
-      result[0].save(function(err){if(err) console.log("a save errorr");});
+    user.find({username:name}).exec(function(error,results1){
+      results[0].buyer=name;
+      results[0].price+=results1[0].addMoney;
+      results[0].save(function(err){if(err) console.log("a save errorr");});
+
     });
+    
+    
   });
 });
 
@@ -245,12 +271,19 @@ app.post('/buy/:id/:name',function(req,res) {
 //post items information to html
 app.post('/upload', upload.single('photo'),
  function(req, res, next){
+  var time= new Date();
+  
+  var addTime=parseInt(req.body.dead);
+   if(req.body.option=='h'){ time.setHours(time.getHours()+addTime);}
+   if(req.body.option=='m'){ time.setMinutes(time.getMinutes()+addTime);}
+   if(req.body.option=='s'){ time.setSeconds(time.getSeconds()+addTime);}
   let u1={
     title:req.body.title,
     description:req.body.desce,
     image:req.file.filename,
     price:req.body.price,
-    stat:req.body.status};
+    deadLine:time,
+    stat:"SALE"};
   
   let name = req.cookies.login.username;
   user.find({username:name})
@@ -263,17 +296,79 @@ app.post('/upload', upload.single('photo'),
   });
 
 });
+//post user information to html
+app.post('/change',upload.single('photo'),function(req,res,next) {
+ 
+  let u = req.cookies.login.username;
+  let add=req.body.addMoney;
+  if(req.file.filename==undefined || add==""){
+    res.send('no name or password or avatar, do again');
+  }
+
+  user.find({username:u}).exec(function(error,results){
+    if(results.length!=0){
+      results[0].addMoney=add;
+      results[0].head=req.file.filename;
+      results[0].save(function(err){if(err) console.log("a save errorr");});
+      res.redirect("index.html");
+      
+
+    }else{
+      res.send('Username already taken, do again');
+    }
+  });
+});
 //You can look for the string
-app.get('/search/:word',function(req,res) {
-
-    let word=req.params.word;
-    items.find({description:{$regex: word} }).exec(function(error,results){
+app.get('/search/:word/:value',function(req,res) {
+  let value=req.params.value;
+  let word=req.params.word;
+  if(value=='all'){
+    items.find({stat:"SALE" }).exec(function(error,results){
       res.send(JSON.stringify(results,null,2));
-    });
-   
-
+     });}
+  if(value=='button'){
+  items.find({title:{$regex: word} }).exec(function(error,results){
+    res.send(JSON.stringify(results,null,2));
+   });}
 });
 
+app.get('/income/:name',function(req,res) {
+
+  let name=req.params.name;
+
+  
+   user.find({username:name}).exec(function(error,results){
+    res.send(JSON.stringify(results[0].incomeList,null,2));
+     });
+  
+});
+
+
+app.post('/add/:user',function(req,res) {
+  let u=req.params.user;
+  let in1= JSON.parse(req.body.income);
+  
+    user.find({username:u}).exec(function(error,results){
+      if(results.length>0){
+        var in2=new income(in1);
+        in2.save(function(err){if(err) console.log("a save errorr");});
+
+        results[0].incomeList.push(in2);
+        results[0].save(function(err){if(err) console.log("a save errorr");});
+        
+        res.send("income add!");
+      }else{
+        res.send("add error");
+      }
+     });
+});
+
+  app.get('/remove/:username',(req,res)=>{
+    let u=req.params.username;
+    user.remove({}).exec(function(error,result){
+      res.send("remove");
+    });
+  })
 //I don't know what this is for
 app.listen(port, function() {
   console.log(`Server running at http://localhost:${port}/`);
